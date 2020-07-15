96d2ea34fadab228f8eb2b26b6293fe41e074164
Anshika-Garg <66871368+Anshika-Garg@users.noreply.github.com>
2020-07-15 23:35:21 +0530

    Update DatabaseConnection.php

--- DatabaseConnection.php
+++ DatabaseConnection.php
@@ -1,8 +1,8 @@
 <?php
-   $hostname="localhost";
-   $un="root";
-   $ps="";
-   $dbname="ecommerce";
+   $hostname=$_ENV["HOSTNAME"];
+   $un=$_ENV["USERNAME"];
+   $ps=$_ENV["PASSWORD"];
+   $dbname=$_ENV["DATABASE"];
      
    $ref= new mysqli($hostname,$un,$ps,$dbname); 
 ?>
